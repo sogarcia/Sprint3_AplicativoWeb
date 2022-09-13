f2c308652becf204af37ab147a4ab4ee532d8b5e
Stiven Orlando Garcia Moreno <sogarcia@asesoftware.com>
2022-09-13 18:40:27 -0500

    commit inicial aplicativo

---
+++ aplicativoWeb/src/main/java/com/mintic/aplicativoWeb/AplicativoWebApplication.java
@@ -0,0 +1,13 @@
+package com.mintic.aplicativoWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicativoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicativoWebApplication.class, args);
+	}
+
+}
---
+++ aplicativoWeb/src/test/java/com/mintic/aplicativoWeb/AplicativoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mintic.aplicativoWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicativoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
